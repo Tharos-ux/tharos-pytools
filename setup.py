c297d96fc8844f9dc0908d67ddc4818ad1ca290d
Siegfried <dubois.siegfried@gmail.com>
2023-09-22 11:42:02 +0200

    fix issue on path func

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='tharos-pytools',
-    version='0.0.14',
+    version='0.0.15',
     description='Collection of quality-of-life functions',
     url='https://github.com/Tharos-ux/tharos-pytools',
     author='Tharos',
--- tharospytools/path_tools.py
+++ tharospytools/path_tools.py
@@ -6,6 +6,7 @@ from pathlib import Path
 def path_allocator(
     path_to_validate: str,
     particle: str | None = None,
+    default_name: str = 'file',
     always_yes: bool = True
 ) -> str:
     """Checks if a file exists in this place, and arborescence exists.
@@ -14,14 +15,17 @@ def path_allocator(
     Args:
         path_to_validate (str): a string path to the file
         particle (str | None, optional): file extension. Defaults to None.
+        default_name (str): a name if name is empty
         always_yes (bool, optional): if file shall be erased by default. Defaults to True.
 
     Returns:
         str: the path to the file, with extension
     """
     folder_path, sep, file_name = path_to_validate.rpartition('/')
+    if file_name == "":
+        file_name = default_name
     if particle and not file_name.endswith(particle):
-        file_name
+        file_name = file_name+particle
     if not always_yes and exists(full_path := (folder_path+sep+file_name)):
         if not input('File already exists. Do you want to write over it? (y/n): ').lower().strip() == 'y':
             raise OSError("File already exists. Aborting.")

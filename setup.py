320483e9f7f5b9e728c94eebe2ee894838be13ab
Siegfried <dubois.siegfried@gmail.com>
2023-10-25 16:56:45 +0200

    distrib

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='tharos-pytools',
-    version='0.0.19',
+    version='0.0.20',
     description='Collection of quality-of-life functions',
     url='https://github.com/Tharos-ux/tharos-pytools',
     author='Tharos',
